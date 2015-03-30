9d04cf5d3469feb19fdc909ac51e8b8e0450ced8
Tomasz Leśniewski <t.lesniewskii@gmail.com>
2015-03-30 22:17:18 +0200

    showing infix from postfix

--- RPN_calculator.py
+++ RPN_calculator.py
@@ -1,4 +1,5 @@
 from reportlab.lib.validators import isNumber
+import sys
 class Calculator:
     rpn = []
     stack = []
@@ -9,22 +10,26 @@ class Calculator:
                   "^":lambda x,y: pow(x, y)
                   }
     
-    def showInputString(self, data):
-        print(data)
-        #for d in data:
-        #    print (d)
+    def showInputString(self):
+        #print(data)
+        sys.stdout.write("Input data is: ")
+        for d in self.rpn:
+            sys.stdout.write("%s, " % d)
+        print("")
+            
         return
     
-    def showAsConventional(self, data):
+    def showAsConventional(self): 
         conventional = []
-        for d in data:
+        for d in self.rpn:
             if isNumber(d):
                 conventional.append(d)
             else:
                 temp1=conventional.pop()
                 temp2=conventional.pop()
-                pass
-                
+                temp3 = "(%s %s %s)" % (temp2, d,temp1)
+                conventional.append(temp3)    
+        print("infix notation: %s" % conventional[-1])  
         return
     
     def isNumber(self, n):
@@ -42,21 +47,30 @@ class Calculator:
             if isNumber(calc):
                 self.stack.append(calc)
             else:
-                temp1=self.stack.pop()
-                temp2=self.stack.pop()
-                result = self.operations[calc](temp2,temp1)
-                #print(result)
-                self.stack.append(result)
+                try:
+                    temp1=self.stack.pop()
+                    temp2=self.stack.pop()
+                    result = self.operations[calc](temp2,temp1)
+                    #print(result)
+                    self.stack.append(result)
+                except IndexError:
+                    print("Index Error - check postfix statement!!")
+                    return
+
+        print("Result = %f" % self.stack[-1])
+
+            
         return
         
         
         
     
 calculator = Calculator()
-#calculator.rpn = [19, 2.14, "+" , 4.5, 2, 4.3, "/", "-", "*"] #85
-calculator.rpn = [12, 2, 3, 4, "*", 10, 5, "/", "+", "*", "+"] #40
-calculator.showAsConventional(calculator.rpn)
-calculator.showInputString(calculator.rpn)
+calculator.rpn = [19, 2.14, "+" , 4.5, 2, 4.3, "/", "-", "*"] #85.297442
+#calculator.rpn = [12, 2, 3, 4, "*", 10, 5, "/", "+", "*", "+"] #40
+
+calculator.showInputString()
+calculator.showAsConventional()
 calculator.calculate(calculator.rpn)
-print (calculator.stack)
+
 
